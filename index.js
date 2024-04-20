30296261fdc527464a112cd5e2bea8bcfef0a1e8
Hugo Oyola <hugo.ham@outlook.com>
2024-04-20 10:31:07 -0500

    Añadir archivos .gitignore, index.js y package.json

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+// Definir puerto
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
+  console.log(`El servidor está funcionando en el puerto ${port}`);
+})
